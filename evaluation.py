598d9981a0f7023dc6ab37fc68f7e104c7400b7e
Chen, Liang Yu (Andymememe) <andymememe@gmail.com>
2020-09-13 20:38:40 +0800

    Add: Predict process

--- evaluation.py
+++ evaluation.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from flask import render_template
 
+from joblib import load
+
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
@@ -10,7 +12,15 @@ def hello_world():
 @app.route('/submit', methods=["POST"])
 def submit():
     comment = request.form.get('comment')
-    return render_template("result.html", Result="Test", Comment=comment)
+    x_test_tfidf = tfidf.transform([comment])
+    y_pred = xgb.predict(x_test_tfidf)
+    if y_pred[0] == 1:
+        result = "Positive"
+    else:
+        result = "Negative"
+    return render_template("result.html", Result=result, Score=y_pred[0], Comment=comment)
 
 if __name__ == "__main__":
+    tfidf = load("model/tfidf.pkl")
+    xgb = load("model/xgb.pkl")
     app.run(debug=True, port=8000, threaded=True)
